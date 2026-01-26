7a48785923f800ce14b414ce63c6a5dff4925a5e
ansaltv1 <ansaltv1@gmail.com>
2026-01-26 16:57:47 +0530

    .

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const canvas = document.getElementById('fireworksCanvas');
 const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+window.addEventListener('resize', resize);
+resize();
+
+// Fireworks Particle System
 let particles = [];
 let fireworksActive = false;
 
@@ -10,39 +16,34 @@ class Particle {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.destX = (Math.random() - 0.5) * 300;
-        this.destY = (Math.random() - 0.5) * 300;
-        this.speed = Math.random() * 0.05 + 0.02;
+        const angle = Math.random() * Math.PI * 2;
+        const velocity = Math.random() * 4 + 2;
+        this.vx = Math.cos(angle) * velocity;
+        this.vy = Math.sin(angle) * velocity;
         this.alpha = 1;
-        this.color = `hsl(${Math.random() * 360}, 100%, 60%)`;
+        this.color = `hsl(${Math.random() * 20 + 200}, 80%, 60%)`;
     }
     update() {
-        this.x += this.destX * this.speed;
-        this.y += this.destY * this.speed;
-        this.alpha -= 0.01;
+        this.x += this.vx;
+        this.y += this.vy;
+        this.alpha -= 0.015;
     }
     draw() {
         ctx.globalAlpha = this.alpha;
         ctx.fillStyle = this.color;
         ctx.beginPath();
-        ctx.arc(this.x, this.y, 3, 0, Math.PI * 2);
+        ctx.arc(this.x, this.y, 2, 0, Math.PI * 2);
         ctx.fill();
     }
 }
 
-function createBurst() {
-    // Burst around the center of the screen
-    for (let i = 0; i < 20; i++) {
-        particles.push(new Particle(canvas.width / 2, canvas.height * 0.4));
-    }
-}
-
 function animate() {
-    ctx.fillStyle = 'rgba(79, 174, 180, 0.79)';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    
-    if (fireworksActive) createBurst();
-
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if (fireworksActive) {
+        for (let i = 0; i < 3; i++) {
+            particles.push(new Particle(canvas.width / 2, canvas.height / 2));
+        }
+    }
     particles.forEach((p, i) => {
         p.update();
         p.draw();
@@ -51,16 +52,12 @@ function animate() {
     requestAnimationFrame(animate);
 }
 
-// THE TIMELINE
+// Start Timeline
+animate();
 setTimeout(() => {
-    // Start fireworks after the 2.5s title animation ends
     fireworksActive = true;
-    animate();
-
-    // Stop fireworks after 2 seconds and show content
     setTimeout(() => {
         fireworksActive = false;
         document.getElementById('portfolioContent').classList.add('show');
     }, 2000);
-
-}, 2500);
+}, 1000);
