dcdf2c6eacb4bd9dbf09c508b3d3a91d17b0ee9d
ansaltv1 <ansaltv1@gmail.com>
2026-01-28 13:09:29 +0530

    .

--- kkkk/script.js
+++ kkkk/script.js
@@ -1,15 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const rows = document.querySelectorAll('.opt-row');
-
-    rows.forEach(row => {
-        // Increases size slightly when mouse enters
-        row.addEventListener('mouseenter', () => {
-            row.style.transform = 'scale(1.03)';
-        });
-
-        // Returns to normal size when mouse leaves
-        row.addEventListener('mouseleave', () => {
-            row.style.transform = 'scale(1)';
-        });
-    });
-});
+let tittle;
+tittle="WEB DEVELOPER";
+console.log(tittle);
